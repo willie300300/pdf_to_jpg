a6377e8847b5ed0778295c3792adc7d1fda3f233
wen lin li <willie300300@yahoo.com.tw>
2019-10-27 10:36:30 +0800

    增加一些文字說明

--- pdf_to_jpg.py
+++ pdf_to_jpg.py
@@ -2,12 +2,14 @@
 
 #需要用到模块wand，这是Imagemagick的Python接口。
 #我们需要用它来将pdf转换为图片
-
+import os
 from wand.image import Image
 from PIL import Image as PIL_Image
 
 def pdf_to_jpg(f):
-    with(Image(filename=f, resolution=1000)) as source: 
+    print('正在讀取資料')
+    with(Image(filename=f, resolution=1000)) as source:
+        print('正在儲存檔案')
         images = source.sequence
         pages = len(images)
         for i in range(pages):
@@ -18,12 +20,13 @@ def pdf_to_jpg(f):
 
 
 def crop_jpg(pagess):
+    print('正在裁切圖片')
     for i in range(pagess):
         n = i + 1
         newfilename = f[:-4] + '-'+ str(n) + '.jpeg'
         im = PIL_Image.open(newfilename)
         img_size = im.size
-        print("圖片寬度和高度分別是{}".format(img_size))
+        print("原始圖片寬度和高度分別是{}".format(img_size))
         x = 0
         y = 0
         w = 1970
@@ -32,10 +35,18 @@ def crop_jpg(pagess):
         region.save(newfilename)
 
 
-f = input('請輸入PDF檔名(例如123.pdf):')
-pagess = pdf_to_jpg(f)
-crop_jpg(pagess)
-       
+
+f = input('請輸入PDF檔名(例如:123.pdf): ')
+if os.path.isfile(f):  # 檢查檔案是否存在
+    print("檔案存在，開始轉檔。")
+    pagess = pdf_to_jpg(f)
+    crop_jpg(pagess)
+
+else:
+    print("檔案不存在。")
+
+
+     
 
 
 #測試上傳
